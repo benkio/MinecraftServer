ae373e7f6973c5ad746c782ac6ba4a71714566e7
benkio <benkio89@gmail.com>
2015-11-20 11:31:11 +0100

    backup

--- LaunchScript/Windows/launch.bat
+++ LaunchScript/Windows/launch.bat
@@ -1,19 +1,19 @@
 REM SEE launch.sh for the dependencies of this script, this is a clone of the unix version.
 
 set telegramScriptPath=%~dp0writeOnTelegram.bat
-set minecraftServerPath= path-to-minecraftserver\minecraft_server.1.8.8.jar
+set minecraftServerPath=C:\Users\Max\Dropbox\Minecraft-Server
 set minecraftGroup=minemerda
 
-set ip_address_string="IP Address"
+rem set ip_address_string="IP Address"
 rem Uncomment the following line when using Windows 7 or Windows 8 / 8.1 (with removing "rem")!
 rem set ip_address_string="IPv4 Address"
-for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
-    set myip=%%f
-    goto Start
-)
+rem for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do (
+rem    set myip=%%f
+rem   goto Start
+rem)
 
 :Start
-
-call %telegramScriptPath% %minecraftGroup% "#ServerUP @ %myip%"
-java -Xmx1024M -Xms1024M -jar %minecraftServerPath% nogui
-call %telegramScriptPath% %minecraftGroup% "#ServerDown @ %myip%"
+cd %minecraftServerPath%
+rem call %telegramScriptPath% %minecraftGroup% "#ServerUP @ %myip%"
+java -Xms1024m -Xmx1024m -jar %minecraftServerPath%\minecraft_server.1.8.8.jar nogui
+rem call %telegramScriptPath% %minecraftGroup% "#ServerDown @ %myip%"
